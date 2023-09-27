6c0e90d9b56c75e93e6a71e0783d30c7fcaf5a2a
Kieran Gray <kieran.gray@gigaclear.com>
2023-09-27 11:34:25 +0100

    revert

--- coding-test/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "SQLALCHEMY_DATABASE_URI", "postgresql://postgres:password@postgres:5432/tails-coding-test-db"
-    )
-    POSTCODES_API_URL = os.getenv("POSTCODES_API_URL", "https://api.postcodes.io/postcodes")
-    LOGGING_LEVEL = "INFO"
